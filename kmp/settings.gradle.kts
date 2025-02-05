f5707d7114762f042d5aac5a1a633876320d5be8
joebad <joe307bad@gmail.com>
2025-02-04 23:48:52 -0500

    Implementing auth

--- kmp/settings.gradle.kts
+++ kmp/settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
         google()
         gradlePluginPortal()
         mavenCentral()
+        maven(url = "https://jitpack.io")
     }
 }
 
--- kmp/shared/build.gradle.kts
+++ kmp/shared/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("org.jetbrains.compose") version "1.7.3"
 }
 
 kotlin {
@@ -26,6 +27,14 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             //put your multiplatform dependencies here
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation("io.github.mirzemehdi:kmpauth-google:2.0.0")
+            implementation("io.github.mirzemehdi:kmpauth-uihelper:2.0.0")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -43,4 +52,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
+dependencies {
+    implementation(libs.androidx.foundation.layout.android)
 }

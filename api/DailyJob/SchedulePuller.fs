d3bd58a1895e3afbc193ea1aa3691b08ada9645a
joebad <joe307bad@gmail.com>
2025-02-09 12:39:07 -0500

    Building out SchedulePuller daily job to populate the next days matchups

---
+++ api/DailyJob/SchedulePuller.fs
@@ -0,0 +1,88 @@
+module SchedulePuller
+
+open System
+open System.Net.Http
+open System.Text.Json
+open MongoDB.Driver
+
+type Team =
+    { id: string
+      displayName: string
+      logo: string }
+
+type Competitor =
+    { id: string
+      homeAway: string
+      winner: bool
+      team: Team }
+
+type Competition = { competitors: Competitor[] }
+
+type Event =
+    { id: string
+      date: string
+      competitions: Competition[] }
+
+type Schedule =
+    { league: string
+      date: string
+      events: Event[] }
+
+let insertSchedule (schedule: Schedule, database: IMongoDatabase, league: string, date: string) =
+    let collection = database.GetCollection<Schedule>("schedules")
+
+    let scheduleWithLeague =
+        { schedule with
+            league = league
+            date = date }
+    
+    let filter =
+        Builders<Schedule>.Filter.And(
+            Builders<Schedule>.Filter.Eq("league", league),
+            Builders<Schedule>.Filter.Eq("date", date)
+        )
+        
+    let updateOptions = ReplaceOptions(IsUpsert = true)
+    
+    let result = collection.ReplaceOne(filter, scheduleWithLeague, updateOptions)
+
+    if result.MatchedCount > 0 then
+        printfn $"Schedule updated successfully. {league} | {date}\n"
+    else
+        printfn $"Schedule inserted successfully. {league} | {date}\n"
+
+let urls =
+    [ ("nba", Printf.StringFormat<string -> string> "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard?dates=%s")
+      ("nhl", Printf.StringFormat<string -> string> "http://site.api.espn.com/apis/site/v2/sports/hockey/nhl/scoreboard?dates=%s")
+      ("nfl", Printf.StringFormat<string -> string> "http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?dates=%s")
+      ("mlb", Printf.StringFormat<string -> string> "http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard?dates=%s") ]
+
+let fetchSchedule (url: string) =
+    async {
+        use client = new HttpClient()
+        let! response = client.GetStringAsync(url) |> Async.AwaitTask
+        return response
+    }
+
+let parseSchedule (json: string) =
+    JsonSerializer.Deserialize<Schedule>(json, JsonSerializerOptions(PropertyNameCaseInsensitive = true))
+
+let pullTomorrowsSchedule (database) =
+    async {
+        let tomorrow =
+            DateTime.UtcNow.AddDays(1).ToString("yyyyMMdd")
+        
+        let runAllAsync date =
+            urls
+            |> List.map (fun (league, espnScheduleEndpoint: Printf.StringFormat<string -> string>) -> async {
+                let url = sprintf espnScheduleEndpoint date
+                let! json = fetchSchedule url
+                let schedule = parseSchedule json
+                insertSchedule (schedule, database, league, date)
+            })
+            |> Async.Parallel
+            |> Async.RunSynchronously
+            
+        runAllAsync tomorrow |> ignore
+    }
+    |> Async.RunSynchronously
--- api/Program.fs
+++ api/Program.fs
@@ -3,8 +3,7 @@ open System.Linq.Expressions
 open Hangfire.Mongo.Migration.Strategies
 open MongoDB.Driver
 open Saturn
-open Saturn.Endpoint
-open api.Todos
+open Schedule
 open Google.Apis.Auth
 open Microsoft.AspNetCore.Http
 open System.Threading.Tasks
@@ -13,18 +12,27 @@ open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.DependencyInjection
 open Hangfire
 open Hangfire.Mongo
+open DotNetEnv
+open Saturn.Endpoint
+open api.Todos 
+
+Env.Load()
+
+let mongoUser = Environment.GetEnvironmentVariable "MONGO_USER"
+let mongoPass = Environment.GetEnvironmentVariable "MONGO_PASS"
+let mongoIp = Environment.GetEnvironmentVariable "MONGO_IP"
+let mongoDb = Environment.GetEnvironmentVariable "MONGO_DB"
 
 let validateGoogleToken (idToken: string) =
     task {
         try
             let! payload = GoogleJsonWebSignature.ValidateAsync(idToken)
             return Some payload
-        with
-        | ex ->
+        with ex ->
             return None
     }
-    
-let requireGoogleAuth : HttpFunc -> HttpContext -> Task<HttpContext option> =
+
+let requireGoogleAuth: HttpFunc -> HttpContext -> Task<HttpContext option> =
     fun next ctx ->
         task {
             match ctx.Request.Headers.TryGetValue("Authorization") with
@@ -42,16 +50,15 @@ let requireGoogleAuth : HttpFunc -> HttpContext -> Task<HttpContext option> =
                     return Some(ctx)
             | _ ->
                 ctx.SetStatusCode 404
-                ctx.WriteJsonAsync("Not found") |> ignore
+                ctx.WriteJsonAsync("Not foiuhiuhund") |> ignore
                 return Some(ctx)
         }
 
 
 
-let api =
-    pipeline {
-        set_header "x-pipeline-type" "API"
-    }
+let api = pipeline {
+    set_header "x-pipeline-type" "API"
+}
 
 let googleAuthPipeline =
     pipeline {
@@ -59,7 +66,82 @@ let googleAuthPipeline =
         set_header "x-pipeline-type" "API"
     }
 
-let defaultRouter =
+
+
+// TOOD use an env var for mongo password, username, ip address, and db
+let mongoConnectionString =
+    $"mongodb://{mongoUser}:{mongoPass}@{mongoIp}:27017/{mongoDb}?directConnection=true&authSource=admin"
+
+let replicaSetName = "rs0"
+
+let configureMongoClientSettings () =
+    let clientSettings = MongoClientSettings.FromConnectionString(mongoConnectionString)
+
+    // Set the replica set name and server selection timeout directly
+    clientSettings.ServerSelectionTimeout <- TimeSpan.FromSeconds(5.0)
+    clientSettings.ReplicaSetName <- replicaSetName
+
+    (new MongoClient(clientSettings), clientSettings)
+
+let (mongoClient, mongoSettings) = configureMongoClientSettings ()
+
+let mongoStorage =
+    MongoStorage(
+        mongoSettings,
+        "fastbreak",
+        MongoStorageOptions(
+            CheckConnection = false,
+            MigrationOptions = MongoMigrationOptions(MigrationStrategy = DropMongoMigrationStrategy())
+        )
+    )
+    
+let database = mongoClient.GetDatabase("fastbreak")
+
+let configureHangfire (services: IServiceCollection) =
+    services.AddHangfire(fun config -> config.UseStorage(mongoStorage) |> ignore)
+    |> ignore
+
+    services.AddHangfireServer() |> ignore
+    services
+
+type JobRunner =
+    static member LogJob() =
+        // TODO implement disabling the daily job based on an env var
+        // pullTomorrowsSchedule(database) |> ignore;
+        printfn "Job executed at %A" DateTime.UtcNow
+
+let scheduleJobs () =
+    let methodCall: Expression<Action<JobRunner>> =
+        Expression.Lambda<Action<JobRunner>>(
+            Expression.Call(typeof<JobRunner>.GetMethod("LogJob")),
+            Expression.Parameter(typeof<JobRunner>, "x")
+        )
+
+    RecurringJob.AddOrUpdate("every-second-job-3", methodCall, "*/1 * * * *")
+
+// let apiRouter = router {
+//     pipe_through api
+//     forward "/schedule" scheduleController
+// }
+
+
+
+let configureApp (app: IApplicationBuilder) =
+    app.UseHangfireDashboard() |> ignore
+    scheduleJobs ()
+    app
+
+let endpointPipe = pipeline {
+    plug fetchSession
+    plug head
+    plug requestId
+}
+
+let scheduleController = router {
+    get "/" (text "Hey")
+}
+
+let defaultRouter = 
     router {
         pipe_through api
         get "/api/todos" Find
@@ -71,57 +153,12 @@ let defaultRouter =
         pipe_through googleAuthPipeline  
         get "/api/todos" Find
     }
-    
-// TOOD use an env var for mongo password, username, ip address, and db
-let mongoConnectionString = "mongodb://USER:PWORD@IP:27017/DB?directConnection=true&authSource=admin"
-let replicaSetName = "rs0"
-
-let configureMongoClientSettings () =
-    let clientSettings = MongoClientSettings.FromConnectionString(mongoConnectionString)
-    
-    // Set the replica set name and server selection timeout directly
-    clientSettings.ServerSelectionTimeout <- TimeSpan.FromSeconds(5.0)
-    clientSettings.ReplicaSetName <- replicaSetName
-
-    clientSettings
-
-let mongoClient = configureMongoClientSettings()
-let configureHangfire (services: IServiceCollection) =
-    let mongoStorage = MongoStorage(mongoClient, "fastbreak", MongoStorageOptions(
-        CheckConnection = false,
-        MigrationOptions = MongoMigrationOptions(
-            MigrationStrategy = DropMongoMigrationStrategy()
-        )
-    ))
-    services.AddHangfire(fun config -> 
-        config.UseStorage(mongoStorage) |> ignore
-    ) |> ignore
-    services.AddHangfireServer() |> ignore
-    services
-    
-type JobRunner =
-    static member LogJob() =
-        printfn "Job executed at %A" DateTime.UtcNow
-
-let scheduleJobs () =
-    let methodCall: Expression<Action<JobRunner>> = 
-        Expression.Lambda<Action<JobRunner>>(
-            Expression.Call(typeof<JobRunner>.GetMethod("LogJob")),
-            Expression.Parameter(typeof<JobRunner>, "x")  // Required parameter
-        )
-    
-    RecurringJob.AddOrUpdate("every-second-job-3", methodCall, "*/1 * * * *")
-
-let configureApp (app: IApplicationBuilder) =
-    app.UseHangfireDashboard() |> ignore
-    scheduleJobs ()  // ✅ Run jobs inside app configuration
-    app
 
 let app =
     application {
+        use_endpoint_router defaultRouter
         service_config configureHangfire
         use_developer_exceptions
-        use_endpoint_router defaultRouter
         app_config configureApp
     }
 

0bb15dfd7945ef678b969c976eb3889cbfab0447
joebad <joe307bad@gmail.com>
2026-04-30 17:33:15 -0400

    Updating ios builder runner env to xcode 26

--- kmp/build.gradle.kts
+++ kmp/build.gradle.kts
@@ -1,3 +1,4 @@
+// Build trigger: rerun TestFlight deploy with Xcode 26.2 path on macos-26 runner.
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
